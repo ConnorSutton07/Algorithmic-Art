d5d7c8998bcde847b61c8cdac8a6009680e7264d
ConnorSutton07 <55513603+ConnorSutton07@users.noreply.github.com>
2020-02-24 17:33:32 -0600

    Create executive.js

---
+++ executive.js
@@ -0,0 +1,10 @@
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let c = canvas.getContext('2d');
+
+
+
+
+//Chaos();
+Balloon();
